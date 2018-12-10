e6e441a3c698743e95ecee8b57750b42fb409957
SilviaAmAm <silvia.amabilino@bristol.ac.uk>
2018-12-10 15:57:01 +0000

    Added a script and modified Readme

---
+++ qml_scripts/Readme.md
@@ -0,0 +1,10 @@
+# General Scripts
+
+This folder contains the scripts used to calculate the results of the paper. It contains 4 types of scripts:
+
+1. Generation of learning curves
+2. Training of a single model and predicting on the test set
+3. Using the single model generated in step 2. for cross predictions
+4. Using the single model to predict the energies of the optimised structures
+
+There is a script for the VR-NN (scripts ending with 'vr') and the MD-NN (scripts ending with 'md').
---
+++ qml_scripts/surface_predictions_md.py
@@ -0,0 +1,48 @@
+"""
+This script uses the model that was trained on the data sampled with the VR to predict the dft energies of the surface.
+"""
+from qml.aglaia.aglaia import ARMP
+import numpy as np
+import h5py
+import time
+
+# Getting the dataset
+
+# Data to predict
+data = h5py.File("cn_isopentane_surface_dft.hdf5", "r")
+
+ene_surface = np.array(data.get("ene"))
+xyz_surface = np.array(data.get("xyz"))
+zs_surface = np.array(data.get("zs"))
+ch_dist_alk = np.array(data.get("ch_dist_alk"))
+ch_dist_cn = np.array(data.get("ch_dist_cn"))
+h_id = np.array(data.get("h_id"))
+
+n_samples = xyz_surface.shape[0]
+
+# Creating the estimator
+acsf_params = {"nRs2":10, "nRs3":10, "nTs":10, "rcut":3.18, "acut":3.18, "zeta":52.779232035094125, "eta":1.4954812022150898}
+
+estimator = ARMP(iterations=5283, batch_size=37, l1_reg=8.931599068573057e-06, l2_reg=3.535679697949907e-05, learning_rate=0.0008170485394812195, representation_name='acsf', representation_params=acsf_params, tensorboard=True, store_frequency=25, hidden_layer_sizes=(15,88))
+
+estimator.load_nn("md-nn")
+estimator.set_properties(ene_surface)
+
+# Generating the representation
+start = time.time()
+estimator.generate_representation(xyz_surface, zs_surface, method="fortran")
+end = time.time()
+print("The time taken to generate the representations is %s s" % (str(end-start)))
+print("The shape of the representations is %s" % (str(estimator.representation.shape)))
+
+idx = list(range(n_samples))
+predictions = estimator.predict(idx)
+
+f = h5py.File("MD-NN_surface_predictions.hdf5", "w")
+f.create_dataset("ch_dist_alk", ch_dist_alk.shape, data=ch_dist_alk)
+f.create_dataset("ch_dist_cn", ch_dist_cn.shape, data=ch_dist_cn)
+f.create_dataset("h_id", h_id.shape, data=h_id)
+f.create_dataset("xyz", xyz_surface.shape, data=xyz_surface)
+f.create_dataset("ene", predictions.shape, data=predictions)
+f.create_dataset("zs", zs_surface.shape, data=zs_surface)
+f.close()
